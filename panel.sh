409f989b510cfc7eaa21a59fc16aa69a4a657750
ytmcnet-byte <ytmc.net@gmail.com>
2026-02-19 22:27:18 +0530

    Update installation script URL in panel.sh

--- panel.sh
+++ panel.sh
@@ -16,7 +16,7 @@ install_ptero() {
     echo "┌──────────────────────────────────────────────┐"
     echo "│        🚀 Pterodactyl Installation            │"
     echo "└──────────────────────────────────────────────┘${NC}"
-    bash <(curl -s https://raw.githubusercontent.com/nobita329/The-Coding-Hub/refs/heads/main/srv/panel/pterodactyl.sh)
+    bash <(curl -s https://raw.githubusercontent.com/ytmcnet-byte/AA-project/refs/heads/main/panels.sh)
     echo -e "${GREEN}✔ Installation Complete${NC}"
     read -p "Press Enter to return..."
 }
